fa41aeb1f46678f853ebfab6f456e5b79ec1fa44
Allan Murray <47799563+AllanBMurray@users.noreply.github.com>
2019-03-15 01:25:32 +0000

    Add files via upload

--- CS230Lab05.php
+++ CS230Lab05.php
@@ -4,11 +4,92 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>CS230 Lab05 - Allan Murray</title>
 <style>
-//body { background: rgba(0,0,0,0.3);}
+.tg {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+.button-style {
+  margin: auto;
+  display: block;
+  color: blue;
+  background-color: grey;
+  font-size: 16px;
+  padding: 10px 10px;
+  border-radius: 9px;
+  border: 2px solid black;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  cursor: pointer;
+  float: left;
+  margin-right: 5px;
+}
+
+.button-style:hover {
+  background-color: darkgray;
+}
 </style>
 
 <script>
-//alert('hi')
+myTable = document.getElementById('myTable');
+function selectRow(){
+//this function waits for a relevant cell to be clicked and then sends the cell or row to another function which highlights the cell or row.
+myTable = document.getElementById('myTable');
+tableCells = myTable.rows[0].cells.length;
+/*
+if (myTable) {
+  for (z = 2; z < tableCells-1; z++) {
+    myTable.rows[0].cells[z].onclick = function() {
+      hlColumn(this); //sends clicked cell to highlight column function.
+    };
+  }
+}*/
+if (myTable) {
+  for (y = 1; y < myTable.rows.length; y++) {
+    for (z = 0; z < myTable.rows[0].cells.length; z++){
+      myTable.rows[y].cells[z].onclick = function() {
+        console.log(this.parentNode.rowIndex);
+        hlRow(this.parentNode); //sends parentNode of the cell clicked. This is the row of the cell.
+      };
+    }  
+  }
+}
+}
+
+function hlRow(selCell) {
+//all cell highlighting is firstly removed. Then all the cells in the row passed are highlighted. The rowToDelete value is set to the index of the row passed.
+for (trhl = 0; trhl < myTable.rows.length; trhl++){
+    for (tchl = 0; tchl < myTable.rows[0].cells.length; tchl++){
+      myTable.rows[trhl].cells[tchl].style.borderWidth = "1px";
+      myTable.rows[trhl].cells[tchl].style.borderColor = "black";
+      myTable.rows[trhl].cells[tchl].style.backgroundColor = "white";
+    }
+  }
+  for (hl = 0; hl < myTable.rows[0].cells.length; hl++){
+    myTable.rows[selCell.rowIndex].cells[hl].style.borderWidth = "3px";
+    myTable.rows[selCell.rowIndex].cells[hl].style.borderColor = "purple";
+    myTable.rows[selCell.rowIndex].cells[hl].style.backgroundColor = "lightgrey";
+  }
+  rowToDelete = selCell.rowIndex;
+  console.log(rowToDelete+"r1d");
+  console.log(myTable.rows[selCell.rowIndex].cells[0].innerHTML);
+  console.log(selCell);
+  document.getElementById("updateid").value = myTable.rows[selCell.rowIndex].cells[0].innerHTML;
+}
+
+function deselectCells(){
+//puts formatting of all cells back to normal including any selected cells. rowToDelete and cellToDelete both set to zero as nothing is now selected.
+  for (trhl = 0; trhl < myTable.rows.length; trhl++){
+    for (tchl = 0; tchl < myTable.rows[0].cells.length; tchl++){
+      myTable.rows[trhl].cells[tchl].style.borderWidth = "1px";
+      myTable.rows[trhl].cells[tchl].style.borderColor = "black";
+    }
+  }
+  rowToDelete = 0;
+  cellToDelete = 0;
+}
+
+//selectColumn(); //react to any columns or rows being selected.
+
 </script>
 </head>
 <body>
@@ -16,10 +97,9 @@
 
   <h1>CS230 Lab05 - Allan Murray</h1>
 
-  <p>Version 1.0</p>
-  <button onclick="submitData()">Submit Data</button>
+  <p>Version 2.0</p>
+  <button onclick="selectRow()">Test Button</button>
  <?php
-
 ?>
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -37,47 +117,41 @@ Language: <label><input type="radio" name="language" id="language" value="Englis
           <label><input type="radio" name="language" id="language" value="Italian">Italian</label>
 <div class="cClearFloat cButtonsUser">
 Description: <input type="text" name="description" id="description"><br>
-<input type="submit" name="addBook" id="addBook" value="Submit Book Information"><br>
-</form>
-
+<input type="submit" class="button-style" name="addBook" id="addBook" value="Submit Book Information"><br>
+</form><br>
+<div>
 <hr>
+<h2>Book Database</h2>
+</div>
 <?php
   $creator = $title = $type = $identifier = $language = $description = "";
   $id = 0;
   $date = date('m/d/Y');
-
-echo "<table style='border: solid 1px black;'>";
+echo "<table class='tg' id='myTable'>";
 echo "<tr><th>Id</th><th>Creator</th><th>Title</th><th>Type</th><th>Identifier</th><th>Date</th><th>Language</th><th>Description</th></tr>";
-
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
         parent::__construct($it, self::LEAVES_ONLY);
     }
-
     function current() {
         return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
     }
-
     function beginChildren() {
         echo "<tr>";
     }
-
     function endChildren() {
         echo "</tr>" . "\n";
     }
 }
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "mysql";
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $conn->prepare("SELECT * FROM eBook_MetaData");
     $stmt->execute();
-
     // set the resulting array to associative
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
@@ -89,9 +163,7 @@ catch(PDOException $e) {
 }
 $conn = null;
 echo "</table>";
-
 if(isset($_POST['addBook'])){
-
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $id = test_input($_POST["id"]); 
     $creator = test_input($_POST["creator"]); 
@@ -101,12 +173,10 @@ if(isset($_POST['addBook'])){
     $date = test_input($_POST["date"]); 
     $language = test_input($_POST["language"]);  
     $description = test_input($_POST["description"]);
-
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "mysql";
-
     //echo "Hello World";
     try {
       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,     $password);
@@ -121,14 +191,10 @@ if(isset($_POST['addBook'])){
     catch(PDOException $e){
       echo $sql . "<br>" . $e->getMessage();
     }
-
     $conn = null;
   
 	
-
-
   }
-
   echo"<h2>Input values:</h2>";
   echo $id;
   echo "<br>";
@@ -147,7 +213,6 @@ if(isset($_POST['addBook'])){
   echo $description;
   echo "<br>";
 }
-
     function test_input($data) {
       $data = trim($data);
       $data = stripslashes($data);
@@ -156,7 +221,57 @@ if(isset($_POST['addBook'])){
     }
 ?>
 
-<h2>Book Database</h2>
-
+<h2>Update Section</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+Update ID: <input type="text" name="updateid" id="updateid"><br>
+<input type="submit" class="button-style" name="updateBook" id="updateBook" value="Update Book Information"><br>
+<!--<p id="updateid"></p>-->
+</form><br>
+<?php
+  //$creator = $title = $type = $identifier = $language = $description = "";
+  //$updateid = 0;
+  //$date = date('m/d/Y');
+  if(isset($_POST['updateBook'])){
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+      $updateid = ($_POST["updateid"]);
+      echo "<table class='tg' id='myUpdate'>";
+      echo "<tr><th>Id</th><th>Creator</th><th>Title</th><th>Type</th><th>Identifier</th><th>Date</th><th>Language</th><th>Description</th></tr>";
+      class updateTableRows extends RecursiveIteratorIterator {
+        function __construct($it) {
+          parent::__construct($it, self::LEAVES_ONLY);
+        }
+        function current() {
+          return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+        }
+        function beginChildren() {
+          echo "<tr>";
+        }
+        function endChildren() {
+        echo "</tr>" . "\n";
+        }
+      }
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $dbname = "mysql";
+      try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT * FROM eBook_MetaData WHERE id = $updateid");
+        $stmt->execute();
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach(new updateTableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+          echo $v;
+        }
+      }
+      catch(PDOException $e) {
+          echo "Error: " . $e->getMessage();
+      }
+      $conn = null;
+      echo "</table>";
+    }
+  }
+?>
 </body>
 </html>
